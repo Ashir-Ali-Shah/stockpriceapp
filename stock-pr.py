4cc2bb296fb3592724fcb08800f2ae5894ff3f0b
Ashir Ali Shah <124299066+Ashir-Ali-Shah@users.noreply.github.com>
2026-03-29 19:10:07 +0500

    Update stock-pr.py

--- stock-pr.py
+++ stock-pr.py
@@ -15,8 +15,6 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.ensemble import ExtraTreesRegressor
 from sklearn.metrics import r2_score, mean_absolute_error
 
-
-
 st.title('Stock Prediction App')
 def main():
     option = st.sidebar.selectbox('Make a choice', ['Visualize','Recent Data', 'Predict'])
@@ -27,14 +25,10 @@ def main():
     else:
         predict()
 
-
-
 @st.cache_resource
 def download_data(op, start_date, end_date):
     df = yf.download(op, start=start_date, end=end_date, progress=False)
     return df
-
-
 
 option = st.sidebar.text_input('Enter a Stock Symbol', value='SPY')
 option = option.upper()
@@ -50,30 +44,20 @@ if st.sidebar.button('Send'):
     else:
         st.sidebar.error('Error: End date must fall after start date')
 
-
-
-
 data = download_data(option, start_date, end_date)
 scaler = StandardScaler()
-
 def tech_indicators():
     st.header('Technical Indicators')
     option = st.radio('Choose a Technical Indicator to Visualize', ['Close', 'BB', 'MACD', 'RSI', 'SMA', 'EMA'])
 
-    # Bollinger bands
     bb_indicator = BollingerBands(data.Close)
     bb = data
     bb['bb_h'] = bb_indicator.bollinger_hband()
     bb['bb_l'] = bb_indicator.bollinger_lband()
-    # Creating a new dataframe
     bb = bb[['Close', 'bb_h', 'bb_l']]
-    # MACD
     macd = MACD(data.Close).macd()
-    # RSI
     rsi = RSIIndicator(data.Close).rsi()
-    # SMA
     sma = SMAIndicator(data.Close, window=14).sma_indicator()
-    # EMA
     ema = EMAIndicator(data.Close).ema_indicator()
 
     if option == 'Close':
@@ -94,14 +78,9 @@ def tech_indicators():
     else:
         st.write('Expoenetial Moving Average')
         st.line_chart(ema)
-
-
 def dataframe():
     st.header('Recent Data')
     st.dataframe(data.tail(10))
-
-
-
 def predict():
     model = st.radio('Choose a model', ['LinearRegression', 'RandomForestRegressor', 'ExtraTreesRegressor', 'KNeighborsRegressor', 'XGBoostRegressor'])
     num = st.number_input('How many days forecast?', value=5)
@@ -122,39 +101,25 @@ def predict():
         else:
             engine = XGBRegressor()
             model_engine(engine, num)
-
-
 def model_engine(model, num):
-    # getting only the closing price
     df = data[['Close']]
-    # shifting the closing price based on number of days forecast
     df['preds'] = data.Close.shift(-num)
-    # scaling the data
     x = df.drop(['preds'], axis=1).values
     x = scaler.fit_transform(x)
-    # storing the last num_days data
     x_forecast = x[-num:]
-    # selecting the required values for training
     x = x[:-num]
-    # getting the preds column
     y = df.preds.values
-    # selecting the required values for training
     y = y[:-num]
 
-    #spliting the data
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=.2, random_state=7)
-    # training the model
     model.fit(x_train, y_train)
     preds = model.predict(x_test)
     st.text(f'r2_score: {r2_score(y_test, preds)} \
             \nMAE: {mean_absolute_error(y_test, preds)}')
-    # predicting stock based on the number of days
     forecast_pred = model.predict(x_forecast)
     day = 1
     for i in forecast_pred:
         st.text(f'Day {day}: {i}')
         day += 1
-
-
 if __name__ == '__main__':
     main()
